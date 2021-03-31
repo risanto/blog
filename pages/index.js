ddfe1d16300eb8a38c4924977e2585c7bc3cb351
risan <potallegta@gmail.com>
2021-03-31 10:36:32 +0700

    Initialise nextjs + tailwindcss

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className={"container"}>
+      <Head>
+        <title>Risan's Blog</title>
+      </Head>
+
+      <main className={""}>
+      </main>
+
+      <footer className={""}>
+      </footer>
+    </div>
+  )
+}
