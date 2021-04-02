3314d5d8527ecd38cd70c7743ff3656af21b381d
risan <potallegta@gmail.com>
2021-04-02 12:26:36 +0700

    Links now open in new tab on click

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -4,7 +4,6 @@ import Layout from '../../components/Layout'
 
 export default function Post(props) {
     const { postData } = props
-    console.log(postData.contentHtml)
 
     function formatImgSrc(thumbnail) {
         return thumbnail.includes('http')
