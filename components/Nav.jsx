400ff8ca7e75245845f34b826a8b4359bb79fe09
risan <potallegta@gmail.com>
2021-04-02 14:56:57 +0700

    Add toggle dark mode button

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,11 @@
 import Link from 'next/link'
+import { useTheme } from 'next-themes'
 
 export default function Nav() {
+    const { theme, setTheme } = useTheme()
+
     return (
-        <nav className={"flex py-2 px-4"}>
+        <nav className={"flex py-2 px-4 justify-between sticky top-0 bg-white dark:bg-black"}>
             <ul className={"flex"}>
                 <li>
                     <Link href="/">
@@ -15,6 +18,24 @@ export default function Nav() {
                     </Link>
                 </li>
             </ul>
+            <button
+                aria-label="Toggle Dark Mode"
+                type="button"
+                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+            >
+                {theme === 'dark' && (
+                    <img
+                        height={25} width={25}
+                        src={"/img/sun.svg"}
+                    />
+                )}
+                {theme === 'light' && (
+                    <img
+                        height={25} width={25}
+                        src={"/img/moon.svg"}
+                    />
+                )}
+            </button>
         </nav>
     )
 }
