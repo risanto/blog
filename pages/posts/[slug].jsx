639e91f5816791c3d103345d8b9efc2ce127bd00
risan <potallegta@gmail.com>
2021-04-24 15:37:53 +0700

    Add scroll to top button

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -1,7 +1,10 @@
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import { getAuthor, getLanguage, getTags } from '../../lib/meta'
+
 import Layout from '../../components/Layout'
 import TagList from '../../components/TagList'
+
+import { useEffect } from 'react'
 
 export default function Post(props) {
     const { postData } = props
@@ -14,12 +17,32 @@ export default function Post(props) {
     let date = new Date(postData.date)
     date = date.toLocaleString('default', { month: 'long', day: 'numeric', year: 'numeric' })
 
+    // When the user clicks on the button, scroll to the top of the document
+    function scrollToTop() {
+        window.scrollTo({top: 0, behavior: 'smooth'})
+    }
+
+    useEffect(() => {
+        const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function () { scrollFunction() };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                scrollToTopBtn.style.display = "block";
+            } else {
+                scrollToTopBtn.style.display = "none";
+            }
+        }
+    }, [])
+
     return (
         <Layout
             pageTitle={postData.title}
             description={postData.description}
             previewImage={postData.thumbnail}
-            // siteName={}
+        // siteName={}
         >
             <header>
                 <h1 className={"font-bold text-4xl dark:text-white"}>{postData.title}</h1>
@@ -44,6 +67,16 @@ export default function Post(props) {
                     { __html: postData.contentHtml }
                 }>
             </article>
+            <button
+                id={"scrollToTopBtn"}
+                className={"fixed bottom-5 right-5"}
+                onClick={scrollToTop}
+            >
+                <img
+                    className={"bg-indigo-50 rounded shadow p-1"}
+                    src={"/img/arrow-up.svg"}
+                />
+            </button>
         </Layout>
     )
 }
