176538ba1753d9b402c5a47594fccf9fe1a78a4a
risan <potallegta@gmail.com>
2021-04-24 12:44:41 +0700

    Change scroll position in TagList based on current query tag

--- components/TagList.jsx
+++ components/TagList.jsx
@@ -1,13 +1,33 @@
 import TagItem from './TagItem'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function TagList({ tags, rowReverse, withBackground }) {
+    const router = useRouter()
+    const routerTag = router.query.tag
+
+    useEffect(() => {
+        const element = routerTag
+            ? document.querySelector(`#taglist-${routerTag}`)
+            : document.querySelector(`#taglist-all`)
+
+        if (element) {
+            element.scrollIntoView({
+                behavior: 'smooth',
+                block: 'nearest',
+                inline: 'center'
+            })
+        }
+    }, [routerTag])
+
     return <ul className={"flex overflow-x-auto " + (rowReverse ? "flex-row-reverse" : "")}>
         {tags.map((tag, idx) => {
             return <li
-                className={"flex " +( withBackground ? "ml-2" : "ml-1")}
+                id={"taglist-" + tag.slug}
+                className={"flex " + (withBackground ? "ml-2" : "ml-1")}
                 key={idx}
             >
-                <TagItem tag={tag} withBackground={withBackground}/>
+                <TagItem tag={tag} withBackground={withBackground} />
             </li>
         })}
     </ul>
