ba60e86bc1c89a6b3705aa25a410c2cfcbb1c9b0
risan <potallegta@gmail.com>
2021-05-02 09:57:12 +0700

    Fix xml error

--- utils/generateSiteMap.js
+++ utils/generateSiteMap.js
@@ -24,11 +24,10 @@ async function generateSiteMap() {
         })
         .join('')
 
-    const sitemap = `
-      <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${pagesMap}
-        </urlset>
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${pagesMap}
+</urlset>
   `
 
     fs.writeFileSync('public/sitemap.xml', sitemap)
