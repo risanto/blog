6257aa280ac1a003f891ce04067699ac28e22952
risan <potallegta@gmail.com>
2021-05-02 09:53:46 +0700

    Fix dynamic sitemap generation

--- utils/generateSiteMap.js
+++ utils/generateSiteMap.js
@@ -2,35 +2,36 @@ const fs = require('fs')
 const globby = require('globby')
 
 async function generateSiteMap() {
-  const pages = await globby([
-    'pages/**/*.js',
-    '!pages/_*.js',
-    '!pages/**/[id].js',
-    '!pages/api',
-    'posts/*.md'
-  ])
+    const pages = await globby([
+        'pages/**/*.jsx',
+        'pages/**/*.md',
+        '!pages/**/_*.jsx',
+        '!pages/**/[slug].jsx'
+    ])
 
-  const sitemap = `
-      <?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-          ${pages
-            .map(page => {
-              const path = page
+    const pagesMap = pages
+        .map(page => {
+            const path = page
                 .replace('pages', '')
-                .replace('.js', '')
+                .replace('.jsx', '')
                 .replace('.md', '')
-              const route = path === '/index' ? '' : path
-              return `
-                      <url>
-                          <loc>${`https://www.risan.dev/${route}`}</loc>
-                      </url>
-                  `
-            })
-            .join('')}
-      </urlset>
+            const route = path === '/index' ? '' : path
+            return `
+              <url>
+                  <loc>${`https://www.risan.dev${route}`}</loc>
+              </url>
+          `
+        })
+        .join('')
+
+    const sitemap = `
+      <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            ${pagesMap}
+        </urlset>
   `
 
-  fs.writeFileSync('public/sitemap.xml', sitemap)
+    fs.writeFileSync('public/sitemap.xml', sitemap)
 }
 
 generateSiteMap()
