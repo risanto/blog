f658a5844d62f1ac5bf5694b2ca9b1b94e728674
Risanto Mulyo <risanto.mulyo@finaccel.co>
2021-05-05 16:25:36 +0700

    Add image for the first project

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -3,7 +3,12 @@ import Layout from '../components/Layout'
 export default function Projects() {
     return (
         <Layout>
-            Projects...
+            <article className={"mt-10"}>
+                <img 
+                className={"transform transition hover:-translate-y-2 hover:duration-500"}
+                src={"/img/cooken-project.png"}
+                />
+            </article>
         </Layout>
     )
 }
