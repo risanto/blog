1a46f5f5cae71fd89b8cee85786d116053a4d45f
Risanto Mulyo <risanto.mulyo@finaccel.co>
2021-05-08 09:21:27 +0700

    Add component ProjectItem and modularise

---
+++ components/ProjectItem.jsx
@@ -0,0 +1,30 @@
+import TechList from '../components/TechList'
+import ProjectLinks from '../components/ProjectLinks'
+
+export default function ProjectItem({ imgSrc, name, description, techList, links }) {
+    return (
+        <article className={"mt-8"}>
+            <img
+                className={"transform transition hover:-translate-y-2 hover:duration-500"}
+                imgAlt={name + ' project'}
+                src={imgSrc}
+            />
+
+            <div className={"transform transition hover:-translate-y-1 hover:duration-500 flex flex-col"}>
+                <div className={"flex mt-4"}>
+                    <h3
+                        className={"border border-indigo-50 bg-indigo-50 self-start z-10 px-1 text-lg relative top-1 dark:text-gray-700 rounded-tl rounded-br"}
+                    >{name}</h3>
+
+                    <TechList names={techList} />
+                </div>
+
+                <div className={"px-4 pt-5 pb-4 border rounded-lg relative -top-3 ml-2 dark:border-gray-700"}>
+                    <p>{description}</p>
+
+                    <ProjectLinks items={links} />
+                </div>
+            </div>
+        </article>
+    )
+}
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,9 +1,35 @@
 import Layout from '../components/Layout'
-import TechList from '../components/TechList'
-import ProjectLinks from '../components/ProjectLinks'
+import ProjectItem from '../components/ProjectItem'
 import Link from 'next/link'
 
 export default function About() {
+    const projects = [
+        {
+            imgSrc: "/img/cooken-project.png",
+            name: "Cooken",
+            description: "An application where users can search for recipes based on the the ingredients they have, using a third-party API to fetch the data.",
+            techList: ["react", "expressjs", "postgres", "sequelize"],
+            links: [
+                {
+                    name: "GIF",
+                    href: "https://s4.gifyu.com/images/cooken.gif"
+                },
+                {
+                    name: "Live site",
+                    href: "https://cooken.netlify.app"
+                },
+                {
+                    name: "Github",
+                    href: "https://github.com/risanto/cooken"
+                },
+                {
+                    name: "Blog",
+                    href: "https://github.com/risanto/cooken"
+                }
+            ],
+        }
+    ]
+
     return (
         <Layout
             pageTitle={"About Risan"}
@@ -42,45 +68,17 @@ export default function About() {
 
             {/* Projects */}
             <section className={"mb-4"}>
-                <article className={"mt-8"}>
-                    <img
-                        className={"transform transition hover:-translate-y-2 hover:duration-500"}
-                        src={"/img/cooken-project.png"}
-                    />
-
-                    <div className={"transform transition hover:-translate-y-1 hover:duration-500 flex flex-col"}>
-                        <div className={"flex mt-4"}>
-                            <h3 className={"border border-indigo-50 bg-indigo-50 self-start z-10 px-1 text-lg relative top-1 dark:text-gray-700 rounded-tl rounded-br"}>Cooken</h3>
-
-                            <TechList names={["react", "expressjs", "postgres", "sequelize"]} />
-                        </div>
-
-                        <div className={"px-4 pt-5 pb-4 border rounded-lg relative -top-3 ml-2 dark:border-gray-700"}>
-                            <p>An application where users can search for recipes based on the the ingredients they have, using a third-party API to fetch the data.</p>
-
-                            <ProjectLinks
-                                items={[
-                                    {
-                                        name: "GIF",
-                                        href: "https://s4.gifyu.com/images/cooken.gif"
-                                    },
-                                    {
-                                        name: "Live site",
-                                        href: "https://cooken.netlify.app"
-                                    },
-                                    {
-                                        name: "Github",
-                                        href: "https://github.com/risanto/cooken"
-                                    },
-                                    {
-                                        name: "Blog",
-                                        href: "https://github.com/risanto/cooken"
-                                    }
-                                ]}
-                            />
-                        </div>
-                    </div>
-                </article>
+                {projects.map(project => {
+                    return (
+                        <ProjectItem
+                            imgSrc={project.imgSrc}
+                            name={project.name}
+                            description={project.description}
+                            techList={project.techList}
+                            links={project.links}
+                        />
+                    )
+                })}
             </section>
         </Layout>
     )
