acd0e049d8ea34e330a25cb48feeac9a9058775a
risan <potallegta@gmail.com>
2021-05-15 15:54:34 +0700

    Change file format from  md to mdx and add chart

--- public/js/sankey.js
+++ public/js/sankey.js
@@ -1,28 +1,36 @@
-google.charts.load("current", { packages: ["sankey"] });
+google.charts.load('current', {
+  'packages': ['sankey']
+});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
   var data = new google.visualization.DataTable();
-  data.addColumn("string", "From");
-  data.addColumn("string", "To");
-  data.addColumn("number", "Weight");
+  data.addColumn('string', 'From');
+  data.addColumn('string', 'To');
+  data.addColumn('number', 'Weight');
   data.addRows([
-    ["A", "X", 5],
-    ["A", "Y", 7],
-    ["A", "Z", 6],
-    ["B", "X", 2],
-    ["B", "Y", 9],
-    ["B", "Z", 4],
+    ['Job applied to: 73', 'Replies: 19', 19],
+    ['Replies: 19', 'Pre-assessments: 5', 5],
+    ['Replies: 19', 'Withdrawn by me: 10', 8],
+    ['Replies: 19', 'Interviews: 9', 6],
+    ['Interviews: 9', 'Offers: 4', 1],
+    ['Interviews: 9', 'Additional interviews: 3', 3],
+    ['Interviews: 9', 'Withdrawn by me: 10', 1],
+    ['Interviews: 9', 'Ghosted: 4', 4],
+    ['Additional interviews: 3', 'Offers: 4', 3],
+    ['Pre-assessments: 5', 'Rejection: 14', 1],
+    ['Pre-assessments: 5', 'Withdrawn by me: 10', 1],
+    ['Pre-assessments: 5', 'Interviews: 9', 3],
+    ['Job applied to: 73', 'Rejection: 14', 13],
+    ['Job applied to: 73', 'No reply: 41', 41],
   ]);
 
   // Sets chart options.
   var options = {
-    width: 600,
+    width: 1000,
   };
 
   // Instantiates and draws our chart, passing in some options.
-  var chart = new google.visualization.Sankey(
-    document.getElementById("sankey_basic")
-  );
+  var chart = new google.visualization.Sankey(document.getElementById('sankey_basic'));
   chart.draw(data, options);
 }
