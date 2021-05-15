dd254e9014457bb4c724c4fc60d1df91d0c2837c
risan <potallegta@gmail.com>
2021-05-15 23:35:17 +0700

    Edit post and margins

--- components/JobHuntChart.jsx
+++ components/JobHuntChart.jsx
@@ -1,6 +1,6 @@
 export default function JobHuntChart() {
     return (
-        <div style={{ marginTop: '1rem', overflowX: 'auto', overflowY: 'hidden' }}>
+        <div style={{ marginTop: '2rem', overflowX: 'auto', overflowY: 'hidden' }}>
             <div id="sankey_basic" style={{ width: '900px', height: '500px' }}></div>
         </div>
     )
