b86933dc837b4044e1a513b4ebe89d2f31cb3368
risan <potallegta@gmail.com>
2021-05-15 16:04:52 +0700

    Add overflow-y: auto

--- components/JobHuntChart.jsx
+++ components/JobHuntChart.jsx
@@ -1,5 +1,5 @@
 export default function JobHuntChart() {
     return (
-        <div id="sankey_basic" style={{ width: '900px', height: '300px' }}></div>
+        <div id="sankey_basic" style={{ width: '900px', height: '300px', marginTop: '1rem', overflowX: 'auto' }}></div>
     )
 }
