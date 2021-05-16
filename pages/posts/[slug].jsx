c30d8c9c4fdf8b126846062cbd455394c246922d
risan <potallegta@gmail.com>
2021-05-16 17:15:59 +0700

    Handle chart not loading while entering from main page

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -71,45 +71,45 @@ export default function Post(props) {
         document.getElementById("progressBar").style.height = scrollPosition + "%"
     }
 
-    // useEffect(() => {
-    //     if (postData.slug === "what-ive-learned-from-three-weeks-of-job hunting" && typeof window !== "undefined") {
-    //         // console.log(new window.google.visualization.DataTable())
-    //         function drawChart() {
-    //             var data = new window.google.visualization.DataTable();
-    //             data.addColumn('string', 'From');
-    //             data.addColumn('string', 'To');
-    //             data.addColumn('number', 'Weight');
-    //             data.addRows([
-    //                 ['Job applied to: 73', 'Replies: 19', 19],
-    //                 ['Replies: 19', 'Pre-assessments: 5', 5],
-    //                 ['Replies: 19', 'Withdrawn by me: 10', 8],
-    //                 ['Replies: 19', 'Interviews: 9', 6],
-    //                 ['Interviews: 9', 'Offers: 4', 1],
-    //                 ['Interviews: 9', 'Additional interviews: 3', 3],
-    //                 ['Interviews: 9', 'Withdrawn by me: 10', 1],
-    //                 ['Interviews: 9', 'Ghosted: 4', 4],
-    //                 ['Additional interviews: 3', 'Offers: 4', 3],
-    //                 ['Pre-assessments: 5', 'Rejection: 14', 1],
-    //                 ['Pre-assessments: 5', 'Withdrawn by me: 10', 1],
-    //                 ['Pre-assessments: 5', 'Interviews: 9', 3],
-    //                 ['Job applied to: 73', 'Rejection: 14', 13],
-    //                 ['Job applied to: 73', 'No reply: 41', 41],
-    //             ]);
+    useEffect(() => {
+        if (postData.slug === "what-ive-learned-from-three-weeks-of-job hunting" && typeof window !== "undefined") {
+
+            function drawChart() {
+                var data = new window.google.visualization.DataTable();
+                data.addColumn('string', 'From');
+                data.addColumn('string', 'To');
+                data.addColumn('number', 'Weight');
+                data.addRows([
+                    ['Job applied to: 73', 'Replies: 19', 19],
+                    ['Replies: 19', 'Pre-assessments: 5', 5],
+                    ['Replies: 19', 'Withdrawn by me: 10', 8],
+                    ['Replies: 19', 'Interviews: 9', 6],
+                    ['Interviews: 9', 'Offers: 4', 1],
+                    ['Interviews: 9', 'Additional interviews: 3', 3],
+                    ['Interviews: 9', 'Withdrawn by me: 10', 1],
+                    ['Interviews: 9', 'Ghosted: 4', 4],
+                    ['Additional interviews: 3', 'Offers: 4', 3],
+                    ['Pre-assessments: 5', 'Rejection: 14', 1],
+                    ['Pre-assessments: 5', 'Withdrawn by me: 10', 1],
+                    ['Pre-assessments: 5', 'Interviews: 9', 3],
+                    ['Job applied to: 73', 'Rejection: 14', 13],
+                    ['Job applied to: 73', 'No reply: 41', 41],
+                ]);
     
-    //             // Sets chart options.
-    //             var options = {
-    //                 width: 900,
-    //                 height: 500
-    //             };
+                // Sets chart options.
+                var options = {
+                    width: 900,
+                    height: 500
+                };
     
-    //             // Instantiates and draws our chart, passing in some options.
-    //             var chart = new window.google.visualization.Sankey(document.getElementById('sankey_basic'));
-    //             chart.draw(data, options);
-    //         }
+                // Instantiates and draws our chart, passing in some options.
+                var chart = new window.google.visualization.Sankey(document.getElementById('sankey_basic'));
+                chart.draw(data, options);
+            }
     
-    //         // drawChart()
-    //     }
-    // }, [])
+            drawChart()
+        }
+    }, [])
 
     useEffect(() => {
         let mounted = true
