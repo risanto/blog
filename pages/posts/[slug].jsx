0a0efdf1a9c55decc4386fa1c46469d371ef3522
risan <potallegta@gmail.com>
2021-05-16 16:42:33 +0700

    Add more condition to ensure google.visualization is loaded first

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -72,7 +72,7 @@ export default function Post(props) {
     }
 
     useEffect(() => {
-        if (postData.slug === "what-ive-learned-from-three-weeks-of-job hunting") {
+        if (postData.slug === "what-ive-learned-from-three-weeks-of-job hunting" && google.visualization) {
             function drawChart() {
                 var data = new google.visualization.DataTable();
                 data.addColumn('string', 'From');
